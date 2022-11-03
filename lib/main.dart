54247611f93df5a692a52a8cb80bd783c695b19f
antonyg5 <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>
2022-11-03 13:18:48 -0300

    initializing the project

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:music/pages/home_music.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: HomeMusic(),
+    ),
+  );
+}
+
---
+++ lib/pages/home_music.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeMusic extends StatefulWidget {
+  const HomeMusic({Key? key}) : super(key: key);
+  @override
+  _HomeMusic createState() => _HomeMusic();
+}
+
+class _HomeMusic extends State <HomeMusic> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("oi");
+  }
+}
