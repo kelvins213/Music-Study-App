8ed47c5e4dec299ab65b8dbd5ed1a906eabb046b
kelvins213 <kelvinotilio@gmail.com>
2022-11-05 00:35:30 -0300

    adding styles on front-end (its so crazying beautiful now my fucking god)

--- lib/data/DBHelper.dart
+++ lib/data/DBHelper.dart
@@ -6,10 +6,10 @@ class DBHelper{
 
   initDB() async {
     String databasePath = await getDatabasesPath();
-    String path = join(databasePath, "music.db");
+    String path = join(databasePath, "music2.db");
     Database database = await openDatabase(
       path,
-      version: 1,
+      version: 2,
       onCreate: onCreate,
       onUpgrade: onUpgrade,
     );
@@ -27,16 +27,21 @@ class DBHelper{
   Future<FutureOr<void>> insertIntoMusicTable(Database db) async {
     String sqlMusic;
 
-    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (0,'Tipo Madara', 'https://youtu.be/wCMzj9Gzyt0?list=RDMM', 'https://images3.alphacoders.com/695/695428.jpg');";
+    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (0,'Come and Go', 'https://youtu.be/SjGVvdq9ZCs', 'https://i0.wp.com/br.nacaodamusica.com/wp-content/uploads/2020/07/Juice-WRLD.jpg');";
     await db.execute(sqlMusic);
-    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (1,'After Party', 'https://youtu.be/GyR38LqBWEE?list=RDMM', 'https://i1.sndcdn.com/artworks-MUe0sOoezI9ORG2U-YTIzhA-t500x500.jpg');";
+    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (1,'Tipo Madara', 'https://youtu.be/wCMzj9Gzyt0?list=RDMM', 'https://images3.alphacoders.com/695/695428.jpg');";
     await db.execute(sqlMusic);
-    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (2,'Feel Like God', 'https://youtu.be/Rxu-SeaLmlo?list=RDMM', 'https://i.ytimg.com/vi/kw-XWS1Usio/sddefault.jpg');";
+    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (2,'After Party', 'https://youtu.be/GyR38LqBWEE?list=RDMM', 'https://i1.sndcdn.com/artworks-MUe0sOoezI9ORG2U-YTIzhA-t500x500.jpg');";
     await db.execute(sqlMusic);
-    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (3,'Drunk and Nasty', 'https://youtu.be/3T5rSK0rd7k?list=RDMM', 'https://m.media-amazon.com/images/I/71iG4caogJL._SS500_.jpg');";
+    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (3,'Feel Like God', 'https://youtu.be/Rxu-SeaLmlo?list=RDMM', 'https://i.ytimg.com/vi/kw-XWS1Usio/sddefault.jpg');";
     await db.execute(sqlMusic);
-    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (4,'Vibe Zabuza', 'https://youtu.be/x6VWj8JeyIU?list=RDMM', 'https://besthqwallpapers.com/Uploads/6-12-2019/114238/thumb2-zabuza-momochi-4k-naruto-characters-momochi-zabuza-manga.jpg');";
+    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (4,'Drunk and Nasty', 'https://youtu.be/3T5rSK0rd7k?list=RDMM', 'https://m.media-amazon.com/images/I/71iG4caogJL._SS500_.jpg');";
     await db.execute(sqlMusic);
+    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (5,'Vibe Zabuza', 'https://youtu.be/x6VWj8JeyIU?list=RDMM', 'https://besthqwallpapers.com/Uploads/6-12-2019/114238/thumb2-zabuza-momochi-4k-naruto-characters-momochi-zabuza-manga.jpg');";
+    await db.execute(sqlMusic);
+    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (6,'Rick and Morty', 'https://youtu.be/br8qyUSbuDI', 'https://static.itapemafm.com.br/s3fs-public/styles/itapema_blog_post_header/public/graphql-upload-files/rick%20and%20morty%20temporada%205_1.jpg');";
+    await db.execute(sqlMusic);
+
   }
 
   Future<void> onUpgrade(Database db, int oldVersion, int newVersion) async {
--- lib/pages/home_music.dart
+++ lib/pages/home_music.dart
@@ -41,79 +41,135 @@ class _HomeMusic extends State <HomeMusic> {
         children: [
           Padding(
             padding: const EdgeInsets.all(16.0),
-            child: FutureBuilder <List<Music>>(
-                future: musicList,
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    List<Music> musics = snapshot.data ?? [];
-                    return ListView.separated(
-                      physics: const NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      itemCount: musics.length,
-                      separatorBuilder: (context, index) {
-                        return const SizedBox(height: 36);
-                      },
-                      itemBuilder: (context, index) {
-                        return Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Color(0xFF000000)),
-                            color: Color(0xFFEEEEEE),
-                          ),
-                          height: 460,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Container(
-                                height: 260,
-                                child: AspectRatio(
-                                  aspectRatio: 4/3,
-                                  child: Image(
-                                      image: NetworkImage(musics[index].imageLink),
-                                  ),
-                                ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(18.0,3.0,18.0,18.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFFEEEEEE),
+                      border: Border.all(color: Color(0xFF000000)),
+                      borderRadius: BorderRadiusDirectional.circular(12),
+                    ),
+                    width: MediaQuery.of(context).size.width - 16,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        returnIcon(icon: Icons.book),
+                        returnIcon(icon: Icons.play_arrow),
+                        returnIcon(icon: Icons.pause_circle),
+                      ],
+                    ),
+                  ),
+                ),
+                FutureBuilder <List<Music>>(
+                    future: musicList,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        List<Music> musics = snapshot.data ?? [];
+                        return ListView.separated(
+                          physics: const NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          itemCount: musics.length,
+                          separatorBuilder: (context, index) {
+                            return const SizedBox(height: 36);
+                          },
+                          itemBuilder: (context, index) {
+                            return Container(
+                              decoration: BoxDecoration(
+                                border: Border.all(color: Color(0xFF000000)),
+                                color: Color(0xFFEEEEEE),
                               ),
-                              Padding(
-                                padding: const EdgeInsets.all(12.0),
-                                child: InkWell(
-                                  child: Container(
-                                    height: 95,
-                                    width: MediaQuery.of(context).size.width - 16,
-                                    color: Color(0xFF777777),
-                                    child: Column(
-                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                      crossAxisAlignment: CrossAxisAlignment.center,
-                                      children: [
-                                        Icon(
-                                          Icons.music_note,
-                                          size: 50,
-                                        ),
-                                        Text(
-                                          '${musics[index].name}',
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 36,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
-                                      ],
+                              height: 460,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Container(
+                                    height: 260,
+                                    child: AspectRatio(
+                                      aspectRatio: 4/3,
+                                      child: CircleAvatar(
+                                        backgroundImage: NetworkImage(musics[index].imageLink),
+                                        child: const Text(""),
+                                      ),
                                     ),
                                   ),
-                                  onTap: () => launchUrlMusic(urlString: musics[index].link),
-                                ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(12.0),
+                                    child: InkWell(
+                                      child: Container(
+                                        height: 95,
+                                        width: MediaQuery.of(context).size.width - 16,
+                                        color: Color(0xFF777777),
+                                        child: Column(
+                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                          crossAxisAlignment: CrossAxisAlignment.center,
+                                          children: [
+                                            Icon(
+                                              Icons.music_note,
+                                              size: 50,
+                                            ),
+                                            Text(
+                                              '${musics[index].name}',
+                                              style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 36,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                      onTap: () => launchUrlMusic(urlString: musics[index].link),
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
+                            );
+                          },
                         );
-                      },
-                    );
-                  }
-                  return const Center(child: CircularProgressIndicator());
-                },
+                      }
+                      return const Center(child: CircularProgressIndicator());
+                    },
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 60,
+            width: MediaQuery.of(context).size.width,
+            color: Color(0xFF000000),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                returnText(text: "Enjoy Your Study Music!", size: 23),
+                returnText(text: "Copyright Ⓒ2022, All Rights Reserved.", size: 14),
+              ],
             ),
           ),
         ],
       ),
     );
   }
+
+  returnText({required String text, required double size}){
+    return Text(
+      text,
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: size,
+        color: Colors.white,
+      ),
+    );
+  }
+
+  returnIcon({required IconData icon}){
+    return Icon(
+      icon,
+      size: 50,
+    );
+  }
+
 }
