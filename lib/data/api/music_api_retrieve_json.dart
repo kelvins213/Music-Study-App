11940b4d5bfc7ee4c9163ce008f33ad51af5ad5f
kelvins213 <kelvinotilio@gmail.com>
2022-12-16 22:24:44 -0300

    api working!

--- lib/data/api/music_api_retrieve_json.dart
+++ lib/data/api/music_api_retrieve_json.dart
@@ -1,17 +1,18 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
+import 'package:music/domain/playlist.dart';
 
-class MusicApi {
+class PlaylistAPI {
 
   final String baseURL = "musicappapi.fhyvugvfjudgf.repl.co";
 
-  Future<List> listDatas() async {
+  Future<List<Playlist>> listDatas() async {
 
     Uri url = Uri.http(baseURL, "/resgatarJSON");
     Response response = await http.get(url);
     List <dynamic> jsonList = <dynamic>[];
+    List <Playlist> playlistContents = <Playlist> [];
 
     print(response.statusCode);
     print(response.body);
@@ -22,9 +23,13 @@ class MusicApi {
       for (var json in result) {
         jsonList.add(json);
       }
+      for (var json in jsonList) {
+        Playlist play = Playlist.fromJson(json);
+        playlistContents.add(play);
+      }
     }
     //essa variável jsonList tem os JSONs retornados pela API em javascript
-    return jsonList;
+    return playlistContents;
   }
 }
 
